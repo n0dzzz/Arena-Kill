eb5392d9865fa8876260e666930eec6ffe140a5c
n0dzzz <62735552+n0dzzz@users.noreply.github.com>
2023-03-11 16:45:17 -0800

    Update cl_hud.lua

--- gamemode/cl_hud.lua
+++ gamemode/cl_hud.lua
@@ -24,6 +24,8 @@ hook.Add("HUDPaint", "ArenaHud", function()
     draw.RoundedBox(4 + 3, 0 + 2.5, surface.ScreenHeight() - 28, surface.ScreenWidth() * (LocalPlayer():GetNWInt("PlayerRankProgress") / LocalPlayer():GetNWInt("MaxPlayerRankProgress")) , 30 - 4, Color(255,255,255, 195))
 
     draw.SimpleTextOutlined("To set walk bind type in your console ] bind key \"+walk\" ", "Trebuchet18", surface.ScreenWidth() * 0.14, surface.ScreenHeight() * 0.02, Color(255, 255, 255, 255), TEXT_ALIGN_CENTER, 0.5, 0.5, Color(45,45,45))
+    draw.SimpleTextOutlined("First to 45 kills wins", "Trebuchet18", surface.ScreenWidth() * 0.062, surface.ScreenHeight() * 0.045, Color(255, 255, 255, 255), TEXT_ALIGN_CENTER, 0.5, 0.5, Color(45,45,45))
+
     draw.SimpleTextOutlined("[Walk Bind] " .. "Open Purchase Menu", "Trebuchet18", surface.ScreenWidth() * 0.09, surface.ScreenHeight() * 0.5, Color(255, 255, 255, 255), TEXT_ALIGN_CENTER, 0.5, 0.5, Color(45,45,45))
 
     draw.SimpleTextOutlined("Level " .. LocalPlayer():GetNWInt("PlayerRank"), "Trebuchet24", surface.ScreenWidth() * 0.04, surface.ScreenHeight() - 55, Color(255, 255, 255, 255), TEXT_ALIGN_CENTER, 2,2, Color(45,45,45))
